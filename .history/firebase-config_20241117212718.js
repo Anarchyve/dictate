1cd3cee216709792ec946213194cb24d5497018d
ANKHO <ankho@ANKHOui-MacBookAir.local>
2024-11-17 21:29:21 +0900

    first init

---
+++ .history/firebase-config_20241117212718.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1LyzOvjdWBJYT3sn_J6jrrWwsZEacEsk",
+    authDomain: "dictate-a704e.firebaseapp.com",
+    projectId: "dictate-a704e",
+    storageBucket: "dictate-a704e.firebasestorage.app",
+    messagingSenderId: "920170290019",
+    appId: "1:920170290019:web:fbaa92923ec620e0429156"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
